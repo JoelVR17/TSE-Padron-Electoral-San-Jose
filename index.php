221a754e9fd3b81886a672a2fe4bc8082dac9b03
JoelVR17 <vermudo.com@gmail.com>
2022-07-05 11:28:47 -0600

    Base
    Se implementa la base del proyecto

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+require 'includes/funciones.php';
+incluirTemplate('header');
+
+?>
+
+<!-- MAIN -->
+<main style="margin: 100px;" class="container">
+    
+</main> <!--fin.main-->
+
+<?php 
+    incluirTemplate('footer');
+?>
---
+++ login.php
@@ -0,0 +1,31 @@
+<!-- HTML5 -->
+<!DOCTYPE html>
+
+<!-- HTML -->
+<html lang="en">
+
+<!-- HEAD -->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/login_registro.css">
+    <title>TSE - Login</title>
+</head> <!--fin.Head-->
+
+<!-- BODY -->
+<body>
+
+    <!-- MAIN -->
+    <main class="container">
+        
+    </main> <!--fin.main-->
+
+</body> <!--fin.body-->
+
+<!-- JAVASCRIPT -->
+<script src="js/script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+</html> <!--fin.html-->
