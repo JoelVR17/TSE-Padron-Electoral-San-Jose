afd9f9ccff75de8f5e7fea602ef66b3fe1d4ed9b
georgeule-cloud <82237911+georgeule-cloud@users.noreply.github.com>
2022-07-18 13:11:25 -0600

    correcion de creacion de tablas y foreign keys

--- g8_proyecto.sql
+++ g8_proyecto.sql
@@ -1,6 +1,7 @@
 /*  Grupo#8   Proyecto Final    SC-504   */
 
 /*  SE CREA TABLA DEL PADRON  */
+
 CREATE TABLE PADRON (
 
     CEDULA varchar2(9),
@@ -11,8 +12,6 @@ CREATE TABLE PADRON (
     APELLIDO2 varchar2(30)
 
 );
-
-/*  SE CREA TABLA DE DISTELEC  */
 CREATE TABLE DISTELEC (
 
     CODELEC number(6),
@@ -20,35 +19,33 @@ CREATE TABLE DISTELEC (
     CANTON varchar2(30),
     DISTRITO varchar2(34)
 
-)
-
-/*  SE CREA TABLA DE BITACORA  */
+);
 CREATE TABLE BITACORA (
 
     ID number PRIMARY KEY ,   
     CEDULA number(9),
     FECHA date
 
-)
+);
 
 /*  SE CREA TABLA DE ROL  */
 CREATE TABLE ROL (
 
-    ID_ROL number(11) PRIMARY KEY,   
+    IDROL number PRIMARY KEY,   
     NOMBRE varchar2(10)
 
-)
+);
 
 /*  SE CREA TABLA DE USUARIO  */
 CREATE TABLE USUARIO (
 
-    ID_USUARIO number primary key,   
+    IDUSUARIO number primary key,   
     USUARIO varchar2(20),
     CONTRASENA varchar2(20),
-    ID_ROL number(11),
-    FOREIGN KEY(ID_ROL) REFERENCES ROL(ID_ROL)
+    IDROL number(11),
+    FOREIGN KEY(IDROL) REFERENCES ROL(IDROL)
 
-)
+);
 
 /*SE HACEN LAS INSERCIONES DE LOS DOS ROLES*/
 INSERT INTO rol 
